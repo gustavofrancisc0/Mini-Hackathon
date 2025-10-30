ac6c1c1e0ee3e314d7ea703812c948d3026341c5
HiagoDz7 <levexhiago@gmail.com>
2025-10-30 01:51:30 -0300

    finalizando sobre

---
+++ views/sobre.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre</title>
+</head>
+
+<body>
+    <section class="about-section">
+        <div class="text-center">
+            <h2 class="mb-4 text-uppercase ">
+                Quem somos
+            </h2>
+        </div>
+        <div class="container">
+            <div class="row align-items-center text-right ">
+                <div class="col-md-6 about-image mb-4 mb-md-0 pb-2">
+                    <img src="../public/assets/img/contrato.jpeg" alt="Sobre Nós" class="img-fluid shadow">
+
+                </div>
+
+                <div class="col-md-6 about-text">
+
+                    <p class="lead">&#x1F4CA A DataCore é uma empresa de tecnologia focada em soluções inteligentes
+                        baseadas em
+                        dados.
+                    <div class="row align-items-center mb-4">
+                        <div class="col-md-8">
+                            <p class="lead">
+                                &#128274 Nosso objetivo
+                                é transformar informações em decisões estratégicas, impulsionando negócios com inovação,
+                                segurança e
+                                eficiência digital.
+                                Com uma equipe especializada em análise de dados, desenvolvimento de software e cloud
+                                computing,
+                                entregamos projetos sob medida para cada cliente.
+                            </p>
+                        </div>
+
+                    </div>
+                    <p class="lead">&#128640 Acreditamos que a tecnologia deve ser simples,
+                        acessível e poderosa — por isso, criamos sistemas que unem velocidade, confiabilidade e design
+                        moderno.
+                        Na DataCore, cada linha de código é pensada para gerar impacto real.
+                        Estamos construindo o futuro da tecnologia, um dado por vez.</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="stats-section text-center text-light py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-6 col-md-3 mb-4">
+                    <h2 class="fw-bold display-5">+15</h2>
+                    <p class="mb-0 ">de Empresas Atendidas</p>
+                </div>
+                <div class="col-6 col-md-3 mb-4">
+                    <h2 class="fw-bold display-5">90%</h2>
+                    <p class="mb-0">Chamados Resolvidos</p>
+                </div>
+                <div class="col-6 col-md-3 mb-4">
+                    <h2 class="fw-bold display 5"> 87%</h2>
+                    <p class="mb-0"> Chamdos Resolvidos Remotamente</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-5">
+                    <h2 class="text-primary mb-5 text-center text-md-start">
+                        O nosso Propósito
+                    </h2>
+                    <p class="lead">Nosso propósito é potencializar a transformação digital por meio do uso inteligente
+                        dos dados.
+                        A DataCore existe para transformar informações em estratégias que geram valor, eficiência e
+                        crescimento
+                        sustentável.
+                        Com tecnologia inovadora, segurança e excelência técnica, capacitamos empresas a tomarem
+                        decisões mais
+                        assertivas e a alcançarem resultados concretos em um mundo cada vez mais orientado por dados.
+                    </p>
+                    <p>Por isso, a DataCore é…,</p>
+                    <p class="fw-bolder">“Tecnologia que Transforma Dados em Estratégia.”</p>
+                    <h3 class="text-primary mt-8 text-center text-md-start">Quer descobrir como a DataCore pode
+                        transformar o seu negócio?
+                        <a href="?page=contato" class="text-decoration-none fw-bold">Fale conosco</a>
+                    </h3>
+                </div>
+                <div class="col-md-6 text-center">
+                    <img src="../public/assets/img/empresa.jpeg" alt="Empresa Fisica" class="img-fluid rounded shadow ">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
